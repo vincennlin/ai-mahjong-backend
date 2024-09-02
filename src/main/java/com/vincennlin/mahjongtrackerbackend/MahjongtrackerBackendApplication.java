0c8642805515a32bf93d36ff6ce151755588e1a5
vincennlin <vincentagwa@gmail.com>
2024-09-02 12:14:24 +0800

    初始化 Project

---
+++ src/main/java/com/vincennlin/mahjongtrackerbackend/MahjongtrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vincennlin.mahjongtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahjongtrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahjongtrackerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincennlin/mahjongtrackerbackend/MahjongtrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincennlin.mahjongtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahjongtrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
